6c46455527e32f0bbafb221e2360e3a71e21610a
jose <jose.silva2000@etec.sp.gov.br>
2022-09-17 10:53:04 -0300

    atividade de media aritmetica

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("media a Aritmetica de 3 numeros:");
+
+Console.WriteLine("digite seu primeiro número:");
+double primeiro = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("digite seu primeiro número:");
+double segundo = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("digite seu primeiro número:");
+double terceiro = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("sua media e : ");
+double media = (primeiro+segundo+terceiro)/3;
+Console.Write($"{media:N1}");
+
